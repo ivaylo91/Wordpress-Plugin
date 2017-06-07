50d5e853322dacee6ca79dabe39081d7c9868b7e
Ivaylo Penev <ipenev91@gmail.com>
2017-06-07 18:11:00 +0300

    Update myplugin.php

--- wp-content/plugins/my_plugin/myplugin.php
+++ wp-content/plugins/my_plugin/myplugin.php
@@ -1,76 +1,34 @@
 <?php
-
 /**
  * Plugin name: My Plugin
  * Description: My First WordPress Plugin
  * Author name: Ivaylo Penev
  */
+define('DXP_VERSION', '1.6');
+define('DXP_PATH', dirname(__FILE__));
+define('DXP_PATH_INCLUDES', dirname(__FILE__) . '/inc');
+define('DXP_FOLDER', basename(DXP_PATH));
+define('DXP_URL', plugins_url() . '/' . DXP_FOLDER);
+define('DXP_URL_INCLUDES', DXP_URL . '/inc');
 
-define( 'DXP_VERSION', '1.6' );
-define( 'DXP_PATH', dirname( __FILE__ ) );
-define( 'DXP_PATH_INCLUDES', dirname( __FILE__ ) . '/inc' );
-define( 'DXP_FOLDER', basename( DXP_PATH ) );
-define( 'DXP_URL', plugins_url() . '/' . DXP_FOLDER );
-define( 'DXP_URL_INCLUDES', DXP_URL . '/inc' );
 
 if (!class_exists('My_Plugin')) {
-
     class My_Plugin
     {
         public function __construct()
         {
             add_action('init', array($this, 'register_episode'));
-
             add_action('add_meta_boxes', array($this, 'meta_box'));
-
             add_action('init', array($this, 'custom_taxonomies'));
-
             add_action('save_post', array($this, 'save'));
-
-            add_action('post_edit_form_tag', array($this, 'save_custom_file'));
-
             add_action('init', array($this, 'shortcode'));
 
-            add_action('widgets_init', array($this, 'simple_event_widget'));
-
-        }
-
-        public function simple_event_widget()
-        {
-            include_once DXP_PATH_INCLUDES . '/event-widget.class.php';
-        }
-
-        public function save_custom_file($post_id)
-        {
-            global $post;
-            if (!empty($_FILES['add_custom_file'])) {
-
-                $file = $_FILES['add_custom_file'];
-
-                $upload = wp_handle_upload($file, array('test_form' => false));
-
-                if (!isset($upload['error']) && isset($upload['error']) != 0) {
-                    $filetype = wp_check_filetype(basename($upload['file']), null);
-                    $title = $file['name'];
-                    $attachment = array(
-                        'post_mime_type' => $filetype,
-                        'post_title' => addslashes_gpc($title),
-                        'post_content' => '',
-                        'post_status' => 'inherit',
-                        'post_parent' => $post->ID
-                    );
-                    $attach_id = wp_insert_attachment($attachment, $upload['file']);
-
-                    update_post_meta($post->ID, 'add_custom_file', $attach_id);
-                }
-            }
         }
 
         public function attach_custom_file($post)
         {
             $file = '';
             if (!empty($post)) {
-
                 $file = get_post_meta($post->id, 'add_custom_file', true);
             }
             ?>
@@ -84,7 +42,6 @@ if (!class_exists('My_Plugin')) {
         function shortcode()
         {
             add_shortcode('episode', array($this, 'shortcode_cb'));
-
         }
 
         public
@@ -93,46 +50,38 @@ if (!class_exists('My_Plugin')) {
             $out = "";
 
             $query = new WP_Query(array(
-
                 'post_type' => 'episode',
                 'post_per_page' => -1
             ));
-
             if ($query->have_posts()) {
-
                 while ($query->have_posts()) {
-
                     $query->the_post();
-
                     $out .= '<p><a href="' . get_the_permalink() . '">' . get_the_title() . '</a></p>';
                 }
             }
-
             return $out;
         }
-
 
         public function save($post_id)
         {
             if (defined('DOING_AJAX') && DOING_AJAX) {
-
                 return $post_id;
             }
             if (isset($_POST['add_info'])) {
 
                 update_post_meta($post_id, 'add_info', $_POST['add_info']);
             }
+
             return $post_id;
         }
 
         public function custom_taxonomies()
         {
-
             register_taxonomy('custom_taxonomies', 'episode', array(
                 'hierarchical' => true,
                 'labels' => array(
-                    'name' => 'Categories',
-                    'singular_name' => 'Category',
+                    'name' => 'Podcast Types',
+                    'singular_name' => 'Podcast',
                     'search_items' => 'Search',
                     'popular_item' => 'Popular',
                     'all_items' => 'All',
@@ -149,34 +98,26 @@ if (!class_exists('My_Plugin')) {
                 'show_ui' => true,
                 'show_admin_column' => true,
                 'query_var' => true,
-                'rewrite' => array('slug' => 'category')
+                'rewrite' => array('slug' => 'podcast type')
             ));
-
-
             register_taxonomy_for_object_type('custom_taxonomies', 'episode');
-
         }
 
         public
         function meta_box()
         {
-
             add_meta_box('meta_box', 'Additional info', array($this, 'meta_box_cb'), 'episode');
 
             add_meta_box('add_custom_file', 'Upload File', array($this, 'attach_custom_file'), 'episode');
         }
 
         public function meta_box_cb($post)
-
         {
             $add_info = " ";
 
             if (!empty($post)) {
-
                 $add_info = get_post_meta($post->ID, 'add_info', true);
-
             }
-
             ?>
             <textarea name="add_info" cols="50" rows="10">
                   <?php echo $add_info; ?>
@@ -193,8 +134,7 @@ if (!class_exists('My_Plugin')) {
                 ),
                 'public' => true,
                 'has archive' => true,
-                'taxonomies' => array('categories', 'post_tag')
-
+                'taxonomies' => array('podcast type', 'post_tag')
             ));
         }
     }
